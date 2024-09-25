c2b0efeff76f403722f8eef8ae2b9ba629c1e3f8
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-09-25 19:30:47 +0530

    redwood: Import XiaomiParts from surya device tree

--- device.mk
+++ device.mk
@@ -132,6 +132,13 @@ $(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-parts.xml
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
