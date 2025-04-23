5c93e80a9de46517b26c9d38e0e3af056672300b
Silesh K Nair <sileshn@gmail.com>
2025-04-23 18:41:07 +0200

    redwood: Move public.libraries.txt to vendor
    Signed-off-by: Thereache <thereache2003@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -53,6 +53,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/vintf/manifest/c2_manifest_vendor.xml': blob_fixup()
         .regex_replace('.*ozoaudio.*\n?', '')
         .regex_replace('.*dolby.*\n?', ''),
+    'vendor/etc/public.libraries.txt': blob_fixup()
+        .regex_replace('.*libqti-perfd-client.so.*\n?', ''),
     'vendor/lib64/android.hardware.secure_element@1.0-impl.so': blob_fixup()
         .remove_needed('android.hidl.base@1.0.so'),
     ('vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
