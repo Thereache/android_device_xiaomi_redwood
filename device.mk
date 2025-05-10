770f8679dc074abc2ace33e8c68a5ea840fde484
Silesh K Nair <sileshn@gmail.com>
2025-05-11 05:14:46 +0530

    redwood: media: Move init files to rootdir

--- device.mk
+++ device.mk
@@ -257,10 +257,6 @@ $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/init.qti.media.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qti.media.sh \
-    $(LOCAL_PATH)/media/init.qti.media.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.qti.media.rc
-
-PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/etc/media_codecs_lahaina.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_lahaina.xml \
     $(LOCAL_PATH)/media/etc/media_codecs_yupik_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_yupik_v1.xml
 
