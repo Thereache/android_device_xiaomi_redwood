e4c41b860a3cff896c2fcae022a381bd0158c285
Silesh K Nair <sileshn@gmail.com>
2025-07-25 10:12:16 +0530

    redwood: miuicamera: cleanup makefile calls
    Signed-off-by: Thereache <thereache2003@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -243,6 +243,3 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit proprietary blobs
 include vendor/xiaomi/redwood/BoardConfigVendor.mk
-
-# Inherit from proprietary files for Leica Camera
--include vendor/xiaomi/redwood-miuicamera/products/board.mk
