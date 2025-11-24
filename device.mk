3b71da6711a1afca878f219265b19efb158b7dcf
Mashopy <eliasgheeraert@gmail.com>
2025-11-24 12:00:00 +0000

    redwood: Remove checkpoint_gc
    Google stopped using it since gs101, the exact reason why is unknown since it was done during early development.
    
    Change-Id: I34c45127db464553eb0f7c287e9039319bf30e34

--- device.mk
+++ device.mk
@@ -86,16 +86,6 @@ PRODUCT_COPY_FILES += \
 
 $(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=ext4 \
-    POSTINSTALL_OPTIONAL_vendor=true
-
-PRODUCT_PACKAGES += \
-    checkpoint_gc
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default
