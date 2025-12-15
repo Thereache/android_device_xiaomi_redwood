f44f605c7cdfc6b2f7e5438a901b3f06753954b8
Thereache <thereache2003@gmail.com>
2025-12-15 22:46:45 +0100

    redwood: update blobs from OS2.0.13.0.UMSMIXM
    Signed-off-by: Thereache <thereache2003@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -196,7 +196,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2025-10-01
+VENDOR_SECURITY_PATCH := 2025-11-01
 
 # Sepolicy
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
