63a32f8ce6e2c71d2be16ae77123590666d7da59
Thereache <thereache2003@gmail.com>
2026-01-07 17:22:47 +0530

    redwood: cleanup and setup
    * tryed to setup extract-files.py & proprietary-files.txt
    
    Revert "redwood: Import XiaomiParts from surya device tree"
    
    This reverts commit c2b0efeff76f403722f8eef8ae2b9ba629c1e3f8.
    
    Revert "redwood: overlay: Switch to vendor-defined color modes"
    
    This reverts commit a77dc42749079c942f7e49e8edbb861996ccd32d.
    
    redwood: Patch configs to drop NFC logging levels
    Signed-off-by: Georg Veichtlbauer <georg@vware.at>
    Change-Id: I42bcd622dea35dd69f4fc6bb72a4767ca69af5c3
    [fiqri19102002: Adapt to python extract utils]
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- device.mk
+++ device.mk
@@ -136,13 +136,6 @@ $(call soong_config_set,camera,package_name,com.android.camera)
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
-# Device Settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-parts.xml
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
