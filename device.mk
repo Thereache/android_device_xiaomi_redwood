e395d410aa15f3a62d975a02ad2f347744bd77cf
Thereache <thereache2003@gmail.com>
2026-01-11 13:30:01 +0530

    redwood: media: remove duplicate codec definitions, handled by vendor
    *
    https://github.com/Redwood-trees/device_xiaomi_redwood/blob/f8f973dd29afd558e1c6669398ac33a4db91c841/proprietary-files.txt#L1073
    https://github.com/Redwood-trees/device_xiaomi_redwood/blob/f8f973dd29afd558e1c6669398ac33a4db91c841/extract-files.py#L58
    https://github.com/Redwood-trees/device_xiaomi_redwood/blob/f8f973dd29afd558e1c6669398ac33a4db91c841/proprietary-files.txt#L1082
    https://github.com/Redwood-trees/device_xiaomi_redwood/blob/f8f973dd29afd558e1c6669398ac33a4db91c841/extract-files.py#L61
    Signed-off-by: Thereache <thereache2003@gmail.com>

--- device.mk
+++ device.mk
@@ -255,11 +255,6 @@ $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/qcom-battery/input_suspend)
 
-# Media
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/etc/media_codecs_lahaina.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_lahaina.xml \
-    $(LOCAL_PATH)/media/etc/media_codecs_yupik_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_yupik_v1.xml
-
 # Network
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml \
