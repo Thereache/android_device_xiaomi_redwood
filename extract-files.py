9506ba9f7013fe1f947e90cbb4b8a6a259708268
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-02-11 01:35:48 +0600

    redwood: Whitelist libmialgoengine.so to load dynamically
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -63,7 +63,8 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/etc/init/android.hardware.drm@1.3-service.widevine.rc', 'vendor/etc/init/vendor.qti.media.c2@1.0-service.rc'): blob_fixup()
         .regex_replace('writepid /dev/cpuset/foreground/tasks', 'task_profiles ProcessCapacityHigh'),
     'vendor/etc/public.libraries.txt': blob_fixup()
-        .regex_replace('.*libqti-perfd-client.so.*\n?', ''),
+        .regex_replace('.*libqti-perfd-client.so.*\n?', '')
+        .regex_replace(r'(?s)^(.*)\Z',r'\1libmialgoengine.so'),
     'vendor/etc/msm_irqbalance.conf': blob_fixup()
         .regex_replace('#arch_timer, arm-pmu, arch_mem_timer', '#arch_timer, arm-pmu, arch_mem_timer, msm_drm, kgsl_3d0_irq')
         .regex_replace('IGNORED_IRQ=27,23,38', 'IGNORED_IRQ=27,23,38,115,332'),
